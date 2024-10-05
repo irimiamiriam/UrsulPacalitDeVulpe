c2d549a9be1e46cb0a4d0231302aff992f4f49e3
Miriam Irimia <irimiamiriam100@gmail.com>
2024-10-05 22:15:11 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace UrsulPacalitDeVulpe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Linen;
+            this.pictureBox.Location = new System.Drawing.Point(158, 65);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(833, 531);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(158, 620);
+            this.progressBar1.Maximum = 4;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(833, 47);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Enabled = false;
+            this.buttonNext.Location = new System.Drawing.Point(1040, 274);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 108);
+            this.buttonNext.TabIndex = 2;
+            this.buttonNext.Text = ">";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Enabled = false;
+            this.buttonPrev.Location = new System.Drawing.Point(39, 265);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonPrev.Size = new System.Drawing.Size(75, 108);
+            this.buttonPrev.TabIndex = 3;
+            this.buttonPrev.Text = "<";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1015, 620);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 47);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "manual";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1140, 706);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Ursul pacalit de vulpe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UrsulPacalitDeVulpe
+{
+    public partial class Form1 : Form
+    {
+        string[] files;
+        int ind=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string workingdirectory =Environment.CurrentDirectory;
+            string projectd= Directory.GetParent(workingdirectory).Parent.FullName;
+            string path= Path.Combine(projectd, "Resurse");
+            files = Directory.GetFiles(path);
+            pictureBox.Image = Image.FromFile(files[0]);
+            timer1.Start();
+        }
+
+        private  void timer1_Tick(object sender, EventArgs e)
+        {
+            if (ind < files.Length - 2)
+            {
+                ind++;
+            }
+            else { ind = 0; }
+            
+           progressBar1.Value = ind;
+           pictureBox.Image = Image.FromFile(files[ind]);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "manual")
+            {
+                timer1.Stop();
+                buttonPrev.Enabled = true;
+                buttonNext.Enabled = true;
+                button3.Text = "auto";
+            }else
+            {
+                timer1.Start();
+                buttonPrev.Enabled = false;
+                buttonNext.Enabled = false;
+                button3.Text = "manual";
+            }
+
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (ind < files.Length - 2)
+            {
+                ind++;
+            }
+           
+
+            progressBar1.Value = ind;
+            pictureBox.Image = Image.FromFile(files[ind]);
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            if (ind>0)
+            {
+                ind--;
+            }
+            
+
+            progressBar1.Value = ind;
+            pictureBox.Image = Image.FromFile(files[ind]);
+        }
+    }
+}
